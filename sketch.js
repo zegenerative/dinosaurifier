ad6112a2dcc5c5ff3acf748befbe71e7ce0ecbec
zegenerative <zegerdevos@gmail.com>
2019-10-30 16:01:34 +0100

    Add styling

--- sketch.js
+++ sketch.js
@@ -12,50 +12,62 @@ function setup() {
   dinoNames = ml5.charRNN('./models/dinomodel/', dinoNamesReady);
   dinoDescriptions = ml5.charRNN('./models/dinodescriptions/', dinoDescriptionsReady);
 
-  // Grab the DOM elements
+  // name
   textInput = select('#textInput');
   lengthSlider = select('#lenSlider');
   tempSlider = select('#tempSlider');
-  button = select('#generate');
+  // description
+  lengthSliderDescr = select('#lenSliderDescr');
+  tempSliderDescr = select('#tempSliderDescr');
+  buttonName = select('#generateName');
+  buttonDescr = select('#generateDescr');
 
   // DOM element events
-  button.mousePressed(generate);
+  buttonName.mousePressed(generateName);
+  buttonDescr.mousePressed(generateDescription);
   lengthSlider.input(updateSliders);
   tempSlider.input(updateSliders);
+  lengthSliderDescr.input(updateSliders);
+  tempSliderDescr.input(updateSliders);
 }
 
 // Update the slider values
 function updateSliders() {
-  select('#length').html(lengthSlider.value());
-  select('#temperature').html(tempSlider.value());
+  select('#lengthName').html(lengthSlider.value());
+  select('#temperatureName').html(tempSlider.value());
+  select('#lengthDescr').html(lengthSliderDescr.value());
+  select('#temperatureDescr').html(tempSliderDescr.value());
 }
 
-function modelReady() {
-  select('#status').html('Model Loaded');
+function dinoNamesReady() {
+  select('#statusName').html('Model Loaded');
+}
+
+function dinoDescriptionsReady() {
+  select('#statusDescr').html('Model Loaded');
 }
 
 // Generate new text
-function generate() {
+function generateName() {
   // prevent starting inference if we've already started another instance
   // TODO: is there better JS way of doing this?
  if(!runningInference) {
     runningInference = true;
 
-    // Update the status log
-    select('#status').html('Generating...');
-
-    // Grab the original text
     let original = textInput.value();
+    // Update the status log
+    select('#statusName').html('Generating...');
+    
+    // Grab the original text
     // Make it to lower case
-    let txt = original
 
     // Check if there's something to send
-    if (txt.length > 0) {
+    if (original.length > 0) {
       // This is what the LSTM generator needs
       // Seed text, temperature, length to outputs
       // TODO: What are the defaults?
       let data = {
-        seed: txt,
+        seed: original,
         temperature: tempSlider.value(),
         length: lengthSlider.value()
       };
@@ -66,10 +78,37 @@ function generate() {
       // When it's done
       function gotData(err, result) {
         // Update the status log
-        select('#status').html('Ready!');
-        select('#result').html((txt + result.sample).toUpperCase());
+        select('#statusName').html('Ready!');
+        select('#nameResult').html((original + result.sample).toUpperCase());
         runningInference = false;
       }
+    }
+  }
+}
+
+function generateDescription() {
+  // prevent starting inference if we've already started another instance
+  // TODO: is there better JS way of doing this?
+  if(!runningInference) {
+    runningInference = true;
+
+    let original = textInput.value();
+    select('#statusDescr').html('Generating...');
+    
+    let data = {
+      seed: original,
+      temperature: tempSliderDescr.value(),
+      length: lengthSliderDescr.value()
+    }
+
+    // Generate text with the charRNN
+    dinoDescriptions.generate(data, gotData);
+
+    // When it's done
+    function gotData(err, result) {
+      select('#statusDescr').html('Ready!');
+      select('#descriptionResult').html(result.sample);
+      runningInference = false;
     }
   }
 }
